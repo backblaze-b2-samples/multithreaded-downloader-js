16b27e57de76c7af6add006eb156035bf9bd3aa4
chpmnrssll <chpmnrssll@gmail.com>
2018-05-22 01:01:58 -0600

    Cleanup, remove service worker

--- index.js
+++
@@ -1,5 +0,0 @@
-(() => {
-  // Pre register & setup service worker before demo pages load. Prevents
-  // missing the 1st fetch event after service worker is installed.
-  const mtd = new MultiThreadedDownloader()
-})()
